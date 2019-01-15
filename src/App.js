59456be3d336bf57b7208a21824c9e86f182379f
ENLOGIC\shridhar.govindaiah <shridhar.govindaiah@enlogic.com>
2019-01-15 12:30:33 -0600

    Project Creation
    Created project initial setup files and install required libraries. Cleaned up unused files.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col text-center display-4">
+            Welcome to DevConnector.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
